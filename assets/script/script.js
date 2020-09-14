720ddb90c24b9becb597f090c3c78ae704b9db6f
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-09-14 22:54:33 +0100

    feat: Added navigation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,24 @@
 // Javascript for My Portfolio
+
+let desktopNav = document.getElementById('headerLinks')
+let getMobileNav = document.getElementById('mobileNav')
+const clonedNav = desktopNav.cloneNode(true)
+const mobileMenuOpen = document.getElementById('mobileMenuOpen')
+const mobileMenuClose = document.getElementById('mobileMenuClose')
+
+function copiedNav() {
+  return getMobileNav.appendChild(clonedNav)
+}
+copiedNav()
+
+function openMenuMobile() {
+  mobileMenuOpen.style.display = 'none'
+  mobileMenuClose.style.display = 'block'
+  getMobileNav.style.display = 'block'
+}
+
+function closeMenuMobile() {
+  mobileMenuOpen.style.display = 'block'
+  mobileMenuClose.style.display = 'none'
+  getMobileNav.style.display = 'none'
+}
