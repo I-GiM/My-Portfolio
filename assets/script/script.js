17b4cc843169548510645ec8ee83f34e4f83c1a0
ikennaoyiih@gmail.com <61995586+I-GiM@users.noreply.github.com>
2020-09-15 15:52:22 +0100

    feat: Added hero section

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,9 @@ let getMobileNav = document.getElementById('mobileNav')
 const clonedNav = desktopNav.cloneNode(true)
 const mobileMenuOpen = document.getElementById('mobileMenuOpen')
 const mobileMenuClose = document.getElementById('mobileMenuClose')
+const aboutLink = document.getElementById('aboutLink')
+const worksLink = document.getElementById('worksLink')
+const contactLink = document.getElementById('contactLink')
 
 function copiedNav() {
   return getMobileNav.appendChild(clonedNav)
@@ -22,3 +25,19 @@ function closeMenuMobile() {
   mobileMenuClose.style.display = 'none'
   getMobileNav.style.display = 'none'
 }
+
+aboutLink.addEventListener('click', () => {
+  aboutLink.classList.add('active-link')
+  worksLink.classList.remove('active-link')
+  contactLink.classList.remove('active-link')
+})
+worksLink.addEventListener('click', () => {
+  worksLink.classList.add('active-link')
+  aboutLink.classList.remove('active-link')
+  contactLink.classList.remove('active-link')
+})
+contactLink.addEventListener('click', () => {
+  contactLink.classList.add('active-link')
+  aboutLink.classList.remove('active-link')
+  worksLink.classList.remove('active-link')
+})
