9e259ade101447a615269177df146c13ed2f4834
ikennaoyiih@gmail.com <ikennaoyiih@gmail.com>
2020-11-20 10:12:41 +0100

    Added projects section

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,14 @@
 
 const navbarMobile = document.getElementById('navbarMobile');
 
+window.onscroll = () => scrollShow()
+function scrollShow() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    navbarMobile.style.display = "flex"
+    if (screen.width >= 1024) {
+      navbarMobile.style.display = 'none'
+    }
+  } else {
+    navbarMobile.style.display = "none"
+  }
+}
